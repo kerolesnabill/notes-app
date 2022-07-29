4f1a82fee5e896c850c1d621f78ac66a566810b3
kerolesnabill <kerolesnabil513@gmail.com>
2022-07-29 09:09:23 +0200

    Create hook for validation

--- src/components/AddNoteForm.jsx
+++ src/components/AddNoteForm.jsx
@@ -1,37 +1,75 @@
-import { useRef } from "react";
+import UseInput from "../hooks/useInput";
 
 const AddNoteForm = ({ onAddNote }) => {
-  const titleRef = useRef("");
-  const contentRef = useRef("");
+  const {
+    value: enteredTitle,
+    isValid: enteredTitleIsValid,
+    hasError: titleInputHasError,
+    valueChangeHandler: titleChangeHandler,
+    valueBlurHandler: titleBlurHandler,
+    reset: resetTitleInput,
+  } = UseInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredContent,
+    isValid: enteredContentIsValid,
+    hasError: contentInputHasError,
+    valueChangeHandler: contentChangeHandler,
+    valueBlurHandler: contentBlurHandler,
+    reset: resetContentInput,
+  } = UseInput((value) => value.trim() !== "");
+
+  let formIsValid = false;
+
+  if (enteredTitleIsValid && enteredContentIsValid) formIsValid = true;
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
+    if (!formIsValid) return;
+
     const note = {
-      id: Math.floor(Math.random() * 10000),
-      title: titleRef.current.value,
-      content: contentRef.current.value,
+      id: new Date(),
+      title: enteredTitle,
+      content: enteredContent,
     };
 
     onAddNote(note);
+    resetTitleInput();
+    resetContentInput();
   };
+
+  const titleClasses = titleInputHasError ? "title invalid" : "title";
+  const contentClasses = contentInputHasError ? "content invalid" : "content";
+
   return (
     <form onSubmit={onSubmitHandler}>
       <div className="note__form">
         <input
-          className="title"
-          ref={titleRef}
+          className={titleClasses}
           type="text"
           placeholder="Title"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+          onBlur={titleBlurHandler}
         />
         <textarea
-          className="content"
-          ref={contentRef}
+          className={contentClasses}
           placeholder="Take a note..."
           rows="8"
           maxLength="500"
+          value={enteredContent}
+          onChange={contentChangeHandler}
+          onBlur={contentBlurHandler}
         ></textarea>
-        <button className="btn">
+        {titleInputHasError && (
+          <p className="text__error">Title must not be empty.</p>
+        )}
+        {contentInputHasError && (
+          <p className="text__error">Note must not be empty.</p>
+        )}
+
+        <button className="btn" disabled={!formIsValid}>
           <span>+</span>
         </button>
       </div>
